d8eb33dda12f215e742dde41ec23b3e9a59a23a7
janneshatzius <jannes.hatzius@gmail.com>
2025-10-24 17:05:01 +0200

    goofy fehler

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/<esd-workshop-ci-cd_esde_jannes_elias>/", // <-- replace with your repo name for GitHub Pages
+  base: "/esd-workshop-ci-cd_esde_jannes_elias/", // <-- replace with your repo name for GitHub Pages
 });
